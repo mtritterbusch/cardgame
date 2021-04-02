246aafb30a7e5de6f12a0d867e37b4ff266095a1
Marc <marc@kicktech.com>
2021-04-02 09:03:46 -0600

    added more testing coverage for draw3 game

--- tests/test_cardgame_draw3.py
+++ tests/test_cardgame_draw3.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from cardgame.classes.cardgame_draw3 import Draw3Game
-from cardgame.classes.cardgame import MaxPlayersHit
+from cardgame.classes.cardgame import MaxPlayersHit, NeedMorePlayers
 
 
 class TestDraw3(TestCase):
@@ -17,6 +17,27 @@ class TestDraw3(TestCase):
 
     def test_setup_game(self):
         draw3 = Draw3Game()
+
+        # if we explicitly specify no players, then
+        # call to setup game should still have no players
+        draw3.setup_game(player_names=None)
+        self.assertEqual(0, len(draw3.get_current_players()))
+
+        # if we add only 1 player and try to start
+        # the game, we should get the exception
+        # telling us we need to add more players
+        draw3.add_player('Skeeter')
+        self.assertRaises(
+            NeedMorePlayers,
+            draw3.start_game
+        )
+
+        # if we remove all players, count should equal 0
+        draw3.remove_all_players()
+        self.assertEqual(0, len(draw3.get_current_players()))
+
+        # if we try to add too many players,
+        # we should get MaxPlayersHit exception
         self.assertRaises(
             MaxPlayersHit,
             draw3.setup_game,
